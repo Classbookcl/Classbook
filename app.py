145b408ca61548f6768acd0989d14b7e6d35bd21
Classbookcl <classbookcl@gmail.com>
2024-07-08 13:57:12 -0400

    últimos cambios

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/como-funciona')
+def como_funciona():
+    return render_template('como_funciona.html')
+
+@app.route('/quienes-somos')
+def quienes_somos():
+    return render_template('quienes_somos.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
