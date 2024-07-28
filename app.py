d5000d2a62a71112c87d6b4defd95e7295204660
Classbookcl <classbookcl@gmail.com>
2024-07-28 01:36:16 -0400

    lanzamiento

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def index():
     return render_template('index.html')
 
 @app.route('/quienes_somos')
-def informacion():
+def quienes_somos():
     return render_template('quienes_somos.html')
+
+@app.route('/como_funciona')
+def como_funciona():
+    return render_template('como_funciona.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
